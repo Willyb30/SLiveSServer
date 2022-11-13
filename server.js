e51992a343a9efdd21fb743432316377d3930d1d
Willyb30 <otengwilliam@live.fr>
2022-11-13 03:29:03 +0100

    test

--- server.js
+++ server.js
@@ -134,6 +134,44 @@ var yetify = require('yetify'),
 
         server = null;
 
+        if(!config)
+        {
+            config={
+                getconfig: {
+                  env: "dev",
+                  isDev: true,
+                },
+                isDev: true,
+                server: {
+                  port: 8888,
+                  "/* secure */": "/* whether this connects via https */",
+                  secure: false,
+                  key: null,
+                  cert: null,
+                  password: null,
+                },
+                rooms: {
+                  "/* maxClients */": "/* maximum number of clients per room. 0 = no limit */",
+                  maxClients: 0,
+                },
+                stunservers: [
+                  {
+                    url: "stun:stun.l.google.com:19302",
+                  },
+                ],
+                turnservers: [
+                  {
+                    urls: [
+                      "turn:your.turn.servers.here",
+                    ],
+                    secret: "turnserversharedsecret",
+                    expiry: 86400,
+                  },
+                ],
+              }
+        }
+
+
         // Create an http(s) server instance to that socket.io can listen to
         if (config.server.secure) {
             server = require('https').Server({
